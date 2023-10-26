933a9c514e9d9b7e42d0dcc5eda6b271459ee68e
root <root@DESKTOP-CSPRQ1M>
2023-10-26 12:22:40 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import DayCounter from './DayCounter'
+
+function App() {
+    return (
+        <div className="App">
+            <DayCounter />
+        </div>
+    )
+}
+
+export default App
---
+++ src/DayCounter.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react"
+class DayCounter extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            targetDate: '',
+            remainingDays: null,
+            selectedDates: [],
+            futureDate: null,
+            numberOfDays: '',
+        }
+    }
+    handleDateChange = (e) => this.setState({ targetDate: e.target.value })
+    handleDaysChange = (e) => this.setState({ numberOfDays: e.target.value })
+    calculateFutureDate = () => {
+        if (this.state.numberOfDays) {
+            const currentDate = new Date()
+            const futureDate = new Date(currentDate)
+            futureDate.setDate(currentDate.getDate() + parseInt(this.state.numberOfDays))
+            this.setState({ futureDate })
+        }
+    }
+    addDate = () => {
+        if (this.state.targetDate) {
+            this.setState((prevState) => ({
+                selectedDates: [...prevState.selectedDates, prevState.targetDate],
+                targetDate: '',
+            }))
+        }
+    }
+    calculateRemainingDays = (targetDate) => {
+        const currentDate = new Date()
+        targetDate = new Date(targetDate)
+        const timeDiff = targetDate - currentDate
+        const daysRemaining = Math.floor(timeDiff / (1000 * 3600 * 24))
+        return daysRemaining
+    }
+    componentDidMount() {
+        setInterval(() => {
+            const { selectedDates } = this.state
+            const remainingDays = selectedDates.map(this.calculateRemainingDays)
+            this.setState({ remainingDays })
+        }, 1000)
+    }
+    render() {
+        const { targetDate, selectedDates, remainingDays, futureDate, numberOfDays } = this.state
+        return (
+            <div className="day-counter">
+                <div className="bg-gray-100 p-6 rounded-lg shadow-lg text-center">
+                    <h1 className="text-3xl font-bold text-blue-500 mb-4">Date Counter</h1>
+                    <div className="mb-4">
+                        <input type="date" value={targetDate} onChange={this.handleDateChange} className="border rounded-lg p-2" />
+                        <button onClick={this.addDate} className="ml-2 bg-blue-500 text-white px-4 py-2 rounded-lg">Add Date</button>
+                    </div>
+                    <div className="mb-4">
+                        <input type="number" value={numberOfDays} onChange={this.handleDaysChange} className="border rounded-lg p-2" />
+                        <button onClick={this.calculateFutureDate} className="ml-2 bg-blue-500 text-white px-4 py-2 rounded-lg">Calculate Future Date</button>
+                    </div>
+                    <div>
+                        {selectedDates.map((date, index) => (
+                            <p key={index} className="text-lg text-gray-700 mt-4 break-words mr-16 sm:mr-24">
+                                <li className="list-none my-4 px-6 py-8 bg-gray-100 rounded-md shadow-md overflow-hidden relative">
+                                    {date}: <span className="text-red-500">{remainingDays[index]} days remaining</span>
+                                </li>
+                            </p>
+                        ))}
+                    </div>
+                    {futureDate && (
+                        <p className="text-lg text-green-600 mt-4 break-words mr-16 sm:mr-24">
+                            <li className="list-none my-4 px-6 py-8 bg-gray-100 rounded-md shadow-md overflow-hidden relative">
+                                Future Date: {futureDate.toDateString()}
+                            </li>
+                        </p>
+                    )}
+                </div>
+            </div>
+        )
+    }
+}
+export default DayCounter
